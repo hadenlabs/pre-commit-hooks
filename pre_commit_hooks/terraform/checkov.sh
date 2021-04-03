6bbd71eddfafc44b188835ed56239fe1fce46985
Luis Mayta <luis@hadenlabs.com>
2021-04-03 01:30:44 -0500

    fix: validation checkov and terraform tfsec (#19)

--- pre_commit_hooks/terraform/checkov.sh
+++ pre_commit_hooks/terraform/checkov.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-set -e
+set -eo pipefail
 
 if ! command -v checkov >/dev/null 2>&1; then
   echo >&2 "checkov is not available on this system."
@@ -7,4 +7,19 @@ if ! command -v checkov >/dev/null 2>&1; then
   exit 1
 fi
 
-checkov -d .
+declare -a paths
+
+index=0
+
+for file_with_path in "$@"; do
+  file_with_path="${file_with_path// /__REPLACED__SPACE__}"
+
+  paths[index]=$(dirname "$file_with_path")
+
+  (( index++ )) || true
+done
+
+for path_uniq in $(echo "${paths[*]}" | tr ' ' '\n' | uniq | sort -u); do
+  path_uniq="${path_uniq//__REPLACED__SPACE__/ }"
+  checkov -d "${path_uniq}"
+done
