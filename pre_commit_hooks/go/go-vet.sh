0a2d42730af4df035ff1f3a1fd06274c1c5476ff
Luis Mayta <luis@hadenlabs.com>
2021-04-02 23:24:15 -0500

    docs: implement dependence go (#19)

--- pre_commit_hooks/go/go-vet.sh
+++ pre_commit_hooks/go/go-vet.sh
@@ -3,7 +3,7 @@ set -e
 
 if ! command -v go >/dev/null 2>&1; then
   echo >&2 "go is not available on this system."
-  echo >&2 "Please install it by running 'brew install'"
+  echo >&2 "Please install it by running 'brew install go'"
   exit 1
 fi
 
