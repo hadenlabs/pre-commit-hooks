c931d2baf4c208202b48a98680412478533dbafa
@slovacus <slovacus@gmail.com>
2021-04-03 01:30:44 -0500

    fix: validation checkov and terraform tfsec (#19)

--- pre_commit_hooks/terraform/terraform_tfsec.sh
+++ pre_commit_hooks/terraform/terraform_tfsec.sh
@@ -8,4 +8,20 @@ if ! command -v tfsec >/dev/null 2>&1; then
   exit 1
 fi
 
-tfsec .
+declare -a paths
+
+index=0
+
+for file_with_path in "$@"; do
+  file_with_path="${file_with_path// /__REPLACED__SPACE__}"
+
+  paths[index]=$(dirname "$file_with_path")
+
+  (( index++ )) || true
+done
+
+for path_uniq in $(echo "${paths[*]}" | tr ' ' '\n' | sort -u); do
+  path_uniq="${path_uniq//__REPLACED__SPACE__/ }"
+
+  tfsec "${path_uniq}"
+done
