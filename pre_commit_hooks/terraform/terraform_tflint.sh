81ad1a34ee3c6c4e08b4c4219e1794c74e9561d7
@slovacus <slovacus@gmail.com>
2021-04-04 02:18:26 -0500

    fix: get options for validation tflint and tfsec (#22)

--- pre_commit_hooks/terraform/terraform_tflint.sh
+++ pre_commit_hooks/terraform/terraform_tflint.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-set -e
+set -eo pipefail
 
 if ! command -v tflint >/dev/null 2>&1; then
   echo >&2 "tflint is not available on this system."
@@ -8,6 +8,86 @@ if ! command -v tflint >/dev/null 2>&1; then
   exit 1
 fi
 
-for file in "$@"; do
-  tflint "$file"
-done
+main() {
+  initialize_
+  parse_cmdline_ "$@"
+  tflint_
+}
+
+initialize_() {
+  # get directory containing this script
+  local dir
+  local source
+  source="${BASH_SOURCE[0]}"
+  while [[ -L $source ]]; do # resolve $source until the file is no longer a symlink
+    dir="$(cd -P "$(dirname "$source")" > /dev/null && pwd)"
+    source="$(readlink "$source")"
+    # if $source was a relative symlink, we need to resolve it relative to the path where the symlink file was located
+    [[ $source != /* ]] && source="$dir/$source"
+  done
+  _SCRIPT_DIR="$(dirname "$source")"
+
+  # shellcheck source=/dev/null
+  source "$_SCRIPT_DIR/../libs/lib_getopt"
+
+}
+
+parse_cmdline_() {
+  declare argv
+  argv=$(getopt -o conf:log:lang:format --long config:,loglevel:,langserver:,format: -- "$@") || return
+
+  eval "set -- ${argv}"
+
+  for argv; do
+    case $argv in
+      -conf | --config)
+        shift
+        ARGS+=("--config=${1}")
+        shift
+        ;;
+      -log | --loglevel)
+        shift
+        ARGS+=("--loglevel=${1}")
+        shift
+        ;;
+      -lang | --langserver)
+        shift
+        ARGS+=("--langserver=${1}")
+        shift
+        ;;
+      -format | --format)
+        shift
+        ARGS+=("--format=${1}")
+        shift
+        ;;
+      --)
+        shift
+        FILES=("$@")
+        break
+        ;;
+    esac
+  done
+
+}
+
+tflint_() {
+  local index=0
+  for file_with_path in "${FILES[@]}"; do
+    file_with_path="${file_with_path// /__REPLACED__SPACE__}"
+
+    paths[index]=$(dirname "$file_with_path")
+
+    (( index++ )) || true
+  done
+
+  for path_uniq in $(echo "${paths[*]}" | tr ' ' '\n' | sort -u); do
+    path_uniq="${path_uniq//__REPLACED__SPACE__/ }"
+    tflint "${ARGS[@]}" "${path_uniq}"
+  done
+}
+
+# global arrays
+declare -a ARGS
+declare -a FILES
+
+[[ ${BASH_SOURCE[0]} != "$0" ]] || main "$@"
