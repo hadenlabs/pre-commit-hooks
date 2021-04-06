eb05f825902ea6c2eeeb28968891d2e1f2df240f
Luis Mayta <luis@hadenlabs.com>
2021-04-05 19:08:11 -0500

    chore: implement configuration gitleaks (#21)

---
+++ pre_commit_hooks/git/gitleaks.sh
@@ -0,0 +1,40 @@
+#!/usr/bin/env bash
+
+set -eo pipefail
+
+if ! command -v gitleaks >/dev/null 2>&1; then
+  echo >&2 "tflint is not available on this system."
+  echo >&2 "Please install it by running 'go get github.com/zricethezav/gitleaks/v7'"
+  exit 1
+fi
+
+main() {
+  initialize_
+  gitleaks_ "$@"
+}
+
+initialize_() {
+  # get directory containing this script
+  local dir
+  local source
+  source="${BASH_SOURCE[0]}"
+  while [[ -L $source ]]; do # resolve $source until the file is no longer a symlink
+    dir="$(cd -P "$(dirname "$source")" > /dev/null && pwd)"
+    source="$(readlink "$source")"
+    # if $source was a relative symlink, we need to resolve it relative to the path where the symlink file was located
+    [[ $source != /* ]] && source="$dir/$source"
+  done
+  _SCRIPT_DIR="$(dirname "$source")"
+
+  # shellcheck source=/dev/null
+  source "$_SCRIPT_DIR/../../libs/github.com/agriffis/pure-getopt/getopt.bash"
+
+}
+
+gitleaks_() {
+  echo "$@"
+  gitleaks "$@"
+  exit 1
+}
+
+[[ ${BASH_SOURCE[0]} != "$0" ]] || main "$@"
