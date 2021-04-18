42a25ca26b10930f10417114babcca947576a5c3
@slovacus <slovacus@gmail.com>
2021-04-18 11:16:36 -0500

    chore: implement validation have go binary (#34)

--- pre_commit_hooks/go/go-fmt.sh
+++ pre_commit_hooks/go/go-fmt.sh
@@ -2,6 +2,12 @@
 
 set -e
 
+if ! command -v go >/dev/null 2>&1; then
+  echo >&2 "go is not available on this system."
+  echo >&2 "Please install it by running 'brew install go'"
+  exit 1
+fi
+
 for file in "$@"; do
   go fmt "./$(dirname "$file")"
 done
