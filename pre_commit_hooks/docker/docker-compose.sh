feb617cb569879b90e87929d45ee3e933d46aa09
@slovacus <slovacus@gmail.com>
2021-04-20 12:22:07 -0500

    fix: validate code docker-compose (#39)

--- pre_commit_hooks/docker/docker-compose.sh
+++ pre_commit_hooks/docker/docker-compose.sh
@@ -9,51 +9,17 @@ if ! command -v docker-compose >/dev/null 2>&1; then
 fi
 
 main() {
-  initialize_
   parse_cmdline_ "$@"
   docker_compose_
 }
 
-initialize_() {
-  # get directory containing this script
-  local dir
-  local source
-  source="${BASH_SOURCE[0]}"
-  while [[ -L $source ]]; do # resolve $source until the file is no longer a symlink
-    dir="$(cd -P "$(dirname "$source")" > /dev/null && pwd)"
-    source="$(readlink "$source")"
-    # if $source was a relative symlink, we need to resolve it relative to the path where the symlink file was located
-    [[ $source != /* ]] && source="$dir/$source"
-  done
-  _SCRIPT_DIR="$(dirname "$source")"
-
-  # shellcheck source=/dev/null
-  source "$_SCRIPT_DIR/../../libs/github.com/agriffis/pure-getopt/getopt.bash"
-
-}
-
 parse_cmdline_() {
-  declare argv
-  argv=$(getopt -- "$@") || return
-
-  eval "set -- ${argv}"
-
-  for argv; do
-    case $argv in
-      --)
-        shift
-        FILES=("${1}")
-        break
-        ;;
-    esac
-  done
-
+  FILES=("$@")
 }
 
 docker_compose_() {
   for file_validate in "${FILES[@]}"; do
-    docker-compose --file "${file_validate}" config --quiet 2>&1 \
-        | sed "/variable is not set. Defaulting/d"
+    docker-compose --file "${file_validate}" config --quiet
   done
 }
 
