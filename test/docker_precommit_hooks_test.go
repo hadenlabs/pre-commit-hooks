bea9dd546dfb3d4de3dd9594bc0efffd73e6e52b
@slovacus <slovacus@gmail.com>
2021-05-04 12:46:42 -0500

    test: implement build and validate apps terraform (#43)

--- test/docker_precommit_hooks_test.go
+++ test/docker_precommit_hooks_test.go
@@ -3,27 +3,57 @@ package test
 import (
 	"testing"
 
+	"strings"
+
 	"github.com/gruntwork-io/terratest/modules/docker"
 	"github.com/stretchr/testify/assert"
 )
 
-func TestPreCommitHooks011Success(t *testing.T) {
+func TestPreCommitHooksBuildSuccess(t *testing.T) {
 	tag := "hadenlabs/pre-commit-hooks:0.1.1"
 	otherOptions := []string{
 		"--no-cache",
 	}
+
 	buildOptions := &docker.BuildOptions{
 		Tags:         []string{tag},
 		OtherOptions: otherOptions,
 	}
 
 	docker.Build(t, "../", buildOptions)
-
 	opts := &docker.RunOptions{
 		Command: []string{
-			"golangci-lint",
+			"bash", "-c",
+			"compgen -c", "|",
+			"sort -u",
 		},
 	}
-	output := docker.Run(t, tag, opts)
-	assert.NotEmpty(t, output, output)
+	outputListApps := docker.Run(t, tag, opts)
+	assert.NotEmpty(t, outputListApps, outputListApps)
+}
+
+func TestPreCommitHooksValidateTerraformSuccess(t *testing.T) {
+	tag := "hadenlabs/pre-commit-hooks:latest"
+	otherOptions := []string{}
+	expectApps := []string{
+		"terraform",
+		"terragrunt",
+	}
+
+	buildOptions := &docker.BuildOptions{
+		Tags:         []string{tag},
+		OtherOptions: otherOptions,
+	}
+
+	docker.Build(t, "../", buildOptions)
+	opts := &docker.RunOptions{
+		Command: []string{
+			"bash", "-c",
+			"compgen -c", "|",
+			"sort -u",
+		},
+	}
+	outputListApps := docker.Run(t, tag, opts)
+	assert.NotEmpty(t, outputListApps, outputListApps)
+	assert.Subset(t, strings.Split(outputListApps, "\n"), expectApps)
 }
