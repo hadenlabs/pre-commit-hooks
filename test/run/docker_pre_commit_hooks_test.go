414a61ef36ed8db34d2358b2752b5694db1f4f18
Luis Mayta <luis@hadenlabs.com>
2021-05-09 21:08:19 -0500

    refactor: name test docker (#44)

--- test/run/docker_pre_commit_hooks_test.go
+++ test/run/docker_pre_commit_hooks_test.go
@@ -4,17 +4,20 @@ import (
 	"testing"
 
 	"github.com/gruntwork-io/terratest/modules/docker"
+	"github.com/hadenlabs/pre-commit-hooks/config"
 	"github.com/stretchr/testify/assert"
 )
 
 func TestPreCommitHooksRunPreCommitSuccess(t *testing.T) {
+	conf := config.Initialize()
+	imageTag := conf.Docker.ImageTagLatest()
 	otherOptions := []string{}
 	buildOptions := &docker.BuildOptions{
 		Tags:         []string{imageTag},
 		OtherOptions: otherOptions,
 	}
 
-	docker.Build(t, "../", buildOptions)
+	docker.Build(t, "../../", buildOptions)
 	opts := &docker.RunOptions{
 		Command: []string{
 			"bash", "-c",
