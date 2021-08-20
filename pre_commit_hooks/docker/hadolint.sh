a2b5f6ee35dd92923184202c41e753bfc8c3bfc1
Luis Mayta <luis@hadenlabs.com>
2021-08-20 16:47:40 -0500

    chore: implement configuration hadolint (#67)

--- pre_commit_hooks/docker/hadolint.sh
+++ pre_commit_hooks/docker/hadolint.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
-set -e
+
+set -eo pipefail
 
 if ! command -v hadolint >/dev/null 2>&1; then
   echo >&2 "hadolint is not available on this system."
@@ -7,4 +8,54 @@ if ! command -v hadolint >/dev/null 2>&1; then
   exit 1
 fi
 
-hadolint "${@}"
+
+main() {
+  initialize_
+  parse_cmdline_ "$@"
+  hadolint_
+}
+
+initialize_() {
+  # get directory containing this script
+  local dir
+  local source
+  source="${BASH_SOURCE[0]}"
+  while [[ -L $source ]]; do # resolve $source until the file is no longer a symlink
+    dir="$(cd -P "$(dirname "$source")" > /dev/null && pwd)"
+    source="$(readlink "$source")"
+    # if $source was a relative symlink, we need to resolve it relative to the path where the symlink file was located
+    [[ $source != /* ]] && source="$dir/$source"
+  done
+  _SCRIPT_DIR="$(dirname "$source")"
+
+  # shellcheck source=/dev/null
+  source "$_SCRIPT_DIR/../../libs/github.com/agriffis/pure-getopt/getopt.bash"
+
+}
+
+parse_cmdline_() {
+  declare argv
+  argv=$(getopt -o c --long config: -- "$@") || return
+
+  eval "set -- ${argv}"
+
+  for argv; do
+    case $argv in
+      -c | --config)
+        shift
+        ARGS+=("--config ${1}")
+        shift
+        ;;
+    esac
+  done
+
+}
+
+hadolint_() {
+  hadolint "${ARGS[@]}"
+}
+
+# global arrays
+declare -a ARGS
+
+[[ ${BASH_SOURCE[0]} != "$0" ]] || main "$@"
