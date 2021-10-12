d2b8bd8d1ceb5914ea120368d8d2fa75f37d5ed7
Luis Mayta <luis@hadenlabs.com>
2021-10-11 20:56:12 -0500

    chore: implement branch default for git leaks (#71)

--- pre_commit_hooks/git/gitleaks.sh
+++ pre_commit_hooks/git/gitleaks.sh
@@ -3,15 +3,33 @@
 set -eo pipefail
 
 if ! command -v gitleaks >/dev/null 2>&1; then
-    echo >&2 "tflint is not available on this system."
-    echo >&2 "Please install it by running 'go get github.com/zricethezav/gitleaks/v7'"
-    exit 1
+  echo >&2 "tflint is not available on this system."
+  echo >&2 "Please install it by running 'go get github.com/zricethezav/gitleaks/v7'"
+  exit 1
 fi
 
 main() {
-    initialize_
-    parse_cmdline_ "$@"
-    gitleaks_
+  initialize_
+  parse_cmdline_ "$@"
+  gitleaks_
+}
+
+initialize_() {
+  # get directory containing this script
+  local dir
+  local source
+  source="${BASH_SOURCE[0]}"
+  while [[ -L $source ]]; do # resolve $source until the file is no longer a symlink
+    dir="$(cd -P "$(dirname "$source")" > /dev/null && pwd)"
+    source="$(readlink "$source")"
+    # if $source was a relative symlink, we need to resolve it relative to the path where the symlink file was located
+    [[ $source != /* ]] && source="$dir/$source"
+  done
+  _SCRIPT_DIR="$(dirname "$source")"
+
+  # shellcheck source=/dev/null
+  source "$_SCRIPT_DIR/../../libs/github.com/agriffis/pure-getopt/getopt.bash"
+
 }
 
 parse_cmdline_() {
@@ -178,26 +196,9 @@ parse_cmdline_() {
 
 }
 
-initialize_() {
-    # get directory containing this script
-    local dir
-    local source
-    source="${BASH_SOURCE[0]}"
-    while [[ -L $source ]]; do # resolve $source until the file is no longer a symlink
-        dir="$(cd -P "$(dirname "$source")" > /dev/null && pwd)"
-        source="$(readlink "$source")"
-        # if $source was a relative symlink, we need to resolve it relative to the path where the symlink file was located
-        [[ $source != /* ]] && source="$dir/$source"
-    done
-    _SCRIPT_DIR="$(dirname "$source")"
-
-    # shellcheck source=/dev/null
-    source "$_SCRIPT_DIR/../../libs/github.com/agriffis/pure-getopt/getopt.bash"
-
-}
 
 gitleaks_() {
-    exec gitleaks "${ARGS[@]}"
+  exec gitleaks "${ARGS[@]}" --branch="$(git symbolic-ref --short HEAD)"
 }
 
 # global arrays
